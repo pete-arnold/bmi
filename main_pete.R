d28715442db1c0f3547f470e42889c4afca74fe0
Pete Arnold <p.t.arnold@swansea.ac.uk>
2022-02-01 16:49:18 +0000

    Next step - and a problem found.

--- main_pete.R
+++ main_pete.R
@@ -7,3 +7,12 @@ source("get_population.R")
 dataset <- get_user_data_choice()
 # Given their choice, set the dataset.
 population <- get_population(dataset)
+
+# Get the height and weight of the individual to show - the user or the character.
+if (dataset == "starwars"){
+  name <- get_character()
+  # !!! get height and weight for a character from the dataset.
+} else {
+  height_weight < get_height_weight()
+}
+  
