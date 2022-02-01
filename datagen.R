dfd1bb45aae20ee5c5d77da89b1cae529e9e33d9
anniewillz <98716038+anniewillz@users.noreply.github.com>
2022-02-01 16:14:40 +0000

    namw

--- datagen.R
+++ datagen.R
@@ -2,7 +2,7 @@
 # Data generation - application
 # ------------------------------------------------------------------------------
 # January 2022
-# Alex Jones & Jason Summers &
+# Alex Jones & Jason Summers & Isinene Onoabhagbe
 # ------------------------------------------------------------------------------
 # We'll be using the tidyverse library for this work for its readability. You
 # could use base-R for many of these operations.
