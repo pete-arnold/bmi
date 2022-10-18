51f0cdb7afad70140e9013a5232277877e881128
tinabiggs <c.m.b.biggs@swansea.ac.uk>
2022-10-18 15:43:53 +0100

    Dataset 5

--- Data_generator.R
+++ Data_generator.R
@@ -12,13 +12,13 @@ cat("You have chosen ",n, "data items in your dataset.")
 #Ideal data
 
 #If (choice == "I") {
-n <- 1000
+n <- 100
   id <- seq(1000, 1000 + n - 1, 1)
-  mean_weight <- 70
-  mean_height <- 175
-  sd_weight <- 14
-  sd_height <- 33
-  gender <- sample(c("Male", "Female"), size=1000, replace=TRUE, prob=c(.49,.51))
+  mean_weight <- 90
+  mean_height <- 168
+  sd_weight <- 40
+  sd_height <- 20
+  gender <- sample(c("Male", "Female"), size=n, replace=TRUE, prob=c(.49,.51))
   weight_kg <- as.integer(rnorm(n, mean= mean_weight, sd=sd_weight))
   height_cm <- as.integer(rnorm(n, mean= mean_height, sd=sd_height))
   dataset <- data.frame(id=id, weight=weight_kg, height=height_cm, gender=gender)
@@ -39,6 +39,6 @@ n <- 1000
 #}
 
 
-write.csv(dataset,"Datafile3.csv", row.names = FALSE)
+write.csv(dataset,"Datafile4.csv", row.names = FALSE)
 
 print(dataset)
